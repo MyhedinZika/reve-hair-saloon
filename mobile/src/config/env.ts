08efaac6ac208bd12caf1ecd523f2546c5bc47cb
Myhedin Zika <myhedin@gjirafa.com>
2026-05-07 21:08:16 +0200

    Latest fixes

--- mobile/src/config/env.ts
+++ mobile/src/config/env.ts
@@ -5,18 +5,20 @@ export interface FirebaseConfig {
   storageBucket: string;
   messagingSenderId: string;
   appId: string;
+  measurementId?: string;
 }
 
 export const firebaseConfig: FirebaseConfig = {
-  apiKey: 'REPLACE_ME',
-  authDomain: 'REPLACE_ME.firebaseapp.com',
-  projectId: 'REPLACE_ME',
-  storageBucket: 'REPLACE_ME.appspot.com',
-  messagingSenderId: 'REPLACE_ME',
-  appId: 'REPLACE_ME',
+  apiKey: 'AIzaSyAlovHia0AHrJYknIV-oqwU-dDrdTA1BS4',
+  authDomain: 'revehairsaloon2026.firebaseapp.com',
+  projectId: 'revehairsaloon2026',
+  storageBucket: 'revehairsaloon2026.firebasestorage.app',
+  messagingSenderId: '1099495106269',
+  appId: '1:1099495106269:web:5a9476dcac5198aa12dd34',
+  measurementId: 'G-DCCQMJYGLK',
 };
 
-export const googleWebClientId = 'REPLACE_ME.apps.googleusercontent.com';
+export const googleWebClientId = '1099495106269-5qscn0d953u8cbqesbv6d003omh23rkm.apps.googleusercontent.com';
 
 export const useEmulator = false;
 export const emulatorHost = 'localhost';
